b98c627f4f6bff783d34faa9a21e660ca8b36a8b
Ennie0719 <enhjinee.pagma@gmail.com>
2026-09-20 17:14:10 -0700

    ignore venv311

--- .venv311/lib/python3.11/site-packages/dynaconf/typed/compat.py
+++
@@ -1,7 +0,0 @@
-from inspect import get_annotations
-from types import UnionType
-
-__all__ = [
-    "get_annotations",
-    "UnionType",
-]
--- .venv311/lib/python3.11/site-packages/dynaconf/typed/exceptions.py
+++
@@ -1,7 +0,0 @@
-from dynaconf.validator import ValidationError
-
-
-class DynaconfSchemaError(Exception): ...
-
-
-__all__ = ["DynaconfSchemaError", "ValidationError"]
